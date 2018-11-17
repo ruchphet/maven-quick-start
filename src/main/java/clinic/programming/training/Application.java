5bae5bc8d345d57db937f2ff004f69a88f2ab6ea
ruchphet <ruchphet@hotmail.com>
2018-11-18 00:38:30 +0700

    reformat Application.java

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -3,23 +3,22 @@ package clinic.programming.training;
 import java.util.ArrayList;
 import org.apache.commons.lang3.StringUtils;
 
-
 public class Application {
-    
-    public Application() {
+
+	public Application() {
 		ArrayList<String> aa = new ArrayList<>();
-        System.out.println ("Inside Application");
-    }
-	
-	public static int countWords(String words){
-		String [] sepWord = StringUtils.split(words, ' ');
+		System.out.println("Inside Application");
+	}
+
+	public static int countWords(String words) {
+		String[] sepWord = StringUtils.split(words, ' ');
 		return (sepWord == null) ? 0 : sepWord.length;
 	}
 
-    // method main(): ALWAYS the APPLICATION entry point
-    public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
-	int count = countWords("I have four words.");
-    }
+	// method main(): ALWAYS the APPLICATION entry point
+	public static void main(String[] args) {
+		System.out.println("Starting Application");
+		Application app = new Application();
+		int count = countWords("I have four words.");
+	}
 }
