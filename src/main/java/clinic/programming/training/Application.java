8a77fe4a52b1cde4456538a6f76fb782bffb6f4d
ruchphet <ruchphet@hotmail.com>
2018-11-17 12:28:25 +0700

    Add apache lang3 to dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,8 @@
 package clinic.programming.training;
 
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
+
 
 public class Application {
     
@@ -8,10 +10,16 @@ public class Application {
 		ArrayList<String> aa = new ArrayList<>();
         System.out.println ("Inside Application");
     }
+	
+	public static int countWords(String words){
+		String [] sepWord = StringUtils.split(words, ' ');
+		return (sepWord == null) ? 0 : sepWord.length;
+	}
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	int count = countWords("I have four words.");
     }
 }
